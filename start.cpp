52ec263c7c61b1134b225d5df317674c5e014e86
HEYKASHIF <kashifmohd269@gmail.com>
2022-07-28 21:16:38 +0530

    hyy folks this demo for myself

---
+++ start.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a;
+  printf("Enter a number sir = ");
+  scanf("%d",&a);
+  printf("%d is your Liked number........", a);
+  printf("Hello Kashif");
+  return 0;
+}
+   
